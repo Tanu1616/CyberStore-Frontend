2856ad1c5b3ae2adeaa2edc72f2f2c45ed04ba4d
Tanushree Pandey <84538958+Tanu1616@users.noreply.github.com>
2023-03-01 21:35:21 +0530

    Update appApi.js

--- src/services/appApi.js
+++ src/services/appApi.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const appApi = createApi({
     reducerPath: "appApi",
-    baseQuery: fetchBaseQuery({ baseUrl:"http://localhost:8080" || "https://cyberstore-api.onrender.com"}),
+    baseQuery: fetchBaseQuery({ baseUrl:"https://cyberstore-api.onrender.com"}),
     
     
     endpoints: (builder) => ({
